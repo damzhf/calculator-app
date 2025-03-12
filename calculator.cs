6fceaf637fbb21ca962b259221547e657a1667fe
damzhf <adamzhafran@students.amikom.ac.id>
2025-03-12 14:00:35 +0700

    Menambahkan fungsi Pengurangan

--- calculator.cs
+++ calculator.cs
@@ -4,7 +4,11 @@ class Calculator
 		
 	}
 	
-	static int Penambahan (int a, int b) {
+	static int Penambahan(int a, int b) {
+		return a + b;
+	}
+	
+	static int Pengurangan(int a, int b) {
 		return a + b;
 	}
 }
